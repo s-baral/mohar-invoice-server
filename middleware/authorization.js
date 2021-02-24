1708479be9268f51c36298b3f8a193485cbc9fd0
Shaksham Baral <shakshambaral@gmail.com>
2021-02-24 16:26:56 +0545

    Added roles parameter in authorization

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
+const USER_ROLES = require("../utils/constants");
 require("dotenv").config();
-
+/*
 module.exports = async (req, res, next) => {
   try {
     const jwtToken = req.header("token");
@@ -17,4 +18,31 @@ module.exports = async (req, res, next) => {
     return res.status(403).json("Not Authorize");
   }
   next();
+};
+*/
+
+module.exports = function authorization(role) {
+  return async (req, res, next) => {
+    try {
+      const jwtToken = req.header("token");
+
+      if (!jwtToken) {
+        return res.json({ status: "NAK", data: "Not Authorize" });
+      }
+
+      const payload = jwt.verify(jwtToken, process.env.jwtSecret);
+
+      if (role !== USER_ROLES.ALL && role !== payload.role) {
+        return res.json({
+          status: "NAK",
+          data: "Invalid Credentials",
+        });
+      }
+      req.id = payload.id;
+    } catch (err) {
+      console.error(err.messsage);
+      return res.status(403).json("Not Authorize");
+    }
+    next();
+  };
 };
