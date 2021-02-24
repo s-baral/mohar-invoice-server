45eed364f8a63fc65c7bcbf8899dc891efc03fc6
Shaksham Baral <shakshambaral@gmail.com>
2021-02-24 16:30:24 +0545

    Added User roles

---
+++ utils/constants.js
@@ -0,0 +1,8 @@
+const USER_ROLES = {
+  ADMIN: "admin",
+  LOADAGENT: "load_agent",
+  INSPECTOR: "inspector",
+  ALL: "all",
+};
+
+module.exports = USER_ROLES;
--- utils/jwtGenerator.js
+++ utils/jwtGenerator.js
@@ -1,9 +1,10 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-function jwtGenerator(id) {
+function jwtGenerator(id, role) {
   const payload = {
     id,
+    role,
   };
 
   return jwt.sign(payload, process.env.jwtSecret, { expiresIn: "1hr" });
